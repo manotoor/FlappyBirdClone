320133e3ea902cbfd71e84863bb8167b8d37b5db
Mano Toor <manotoor@gmail.com>
2018-09-24 17:47:05 -0700

    My First Commit

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public delegate void GameDelegate();
+	public static event GameDelegate OnGameStarted;
+	public static event GameDelegate OnGameOverConfirmed;
+
+	public static GameManager Instance;
+
+	public GameObject startPage;
+	public GameObject gameOverPage;
+	public GameObject countDownPage;
+	public Text scoreText;
+
+	enum PageState{
+		None,
+		Start,
+		GameOver,
+		Countdown
+	}
+
+	int score = 0;
+	bool gameOver = false;
+
+	public bool GameOver { get { return gameOver; } }
+
+	void Awake(){
+		Instance = this;
+	}
+
+	void OnEnable(){
+		CountdownText.OnCountdownFinished += OnCountdownFinished;
+		TapController.OnPlayerDied += OnPlayerDied;
+		TapController.OnPlayerScored += OnPlayerScored;
+	}
+	void OnDisable(){
+		CountdownText.OnCountdownFinished -= OnCountdownFinished;
+		TapController.OnPlayerDied -= OnPlayerDied;
+		TapController.OnPlayerScored -= OnPlayerScored;
+	}
+
+	void OnCountdownFinished(){
+		SetPageState(PageState.None);
+		OnGameStarted();	//Event sent to Tap Controller
+		score = 0;
+		gameOver = false;
+	}
+	void OnPlayerDied(){
+		gameOver = true;
+		int savedScore = PlayerPrefs.GetInt("HighScore");
+		if(score > savedScore){
+			PlayerPrefs.SetInt("HighScore", score);
+		}
+		SetPageState(PageState.GameOver);
+	}
+	void OnPlayerScored(){
+		score++;
+		scoreText.text = score.ToString();
+	}
+
+	void SetPageState(PageState state){
+		switch(state){
+			case PageState.None:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(false);
+				countDownPage.SetActive(false);
+				break;
+			case PageState.Start:
+				startPage.SetActive(true);
+				gameOverPage.SetActive(false);
+				countDownPage.SetActive(false);
+				break;
+			case PageState.GameOver:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(true);
+				countDownPage.SetActive(false);
+				break;
+			case PageState.Countdown:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(false);
+				countDownPage.SetActive(true);	
+				break;
+		}
+	}
+
+	public void ConfirmGameOver(){
+		//activated when replay button is hit
+		OnGameOverConfirmed();	//Event sent to Tap Controller
+		scoreText.text = "0";
+		SetPageState(PageState.Start);
+	}
+	public void StartGame(){
+		//activated when play button is hit
+		SetPageState(PageState.Countdown);
+	}
+}
